984792c3ce796db7e96b3c96da653f75b74d12ca
rozsabence <robence@gmail.com>
2019-02-20 20:23:17 +0100

    add layout and finish mvp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
 
+import Table from "./layout/table";
+import Controls from "./layout/controls";
+import Counter from "./layout/counter";
+
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Game of Life (React + Redux)</h1>
+        <Table />
+        <Controls />
+        <Counter />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { createStore, applyMiddleware } from "redux";
 import App from "./App";
 import { Provider } from "react-redux";
 import reducers from "./reducers";
+import "./main.css";
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
