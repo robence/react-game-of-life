df73cc92abaf16a1f58d1a3291b2b2c693154ec4
rozsabence <robence@gmail.com>
2019-02-20 20:21:04 +0100

    add components

---
+++ src/components/cell.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default ({ alive, newBorn, handleClick }) => (
+  <td
+    onClick={handleClick}
+    className={`${alive ? "alive" : ""} ${newBorn ? "new-born" : ""}`}
+  />
+);
