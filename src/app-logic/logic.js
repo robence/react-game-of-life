11c73d4169b617735529a6b6c31341c5e4a389d5
rozsabence <robence@gmail.com>
2019-02-21 13:37:45 +0100

    refactor application logic

--- src/app-logic/logic.js
+++ src/app-logic/logic.js
@@ -1,19 +1,9 @@
+import * as utils from './utils';
+
 export const makeGrid = (height, width, makeRandom = false) => {
-  let grid = [];
-  for (var i = 0; i < height; i++) {
-    var row = [];
-    for (var j = 0; j < width; j++) {
-      let value;
-      if (makeRandom) {
-        value = Math.random() > 0.85;
-      }
-      row.push({
-        status: value,
-        newBorn: value,
-      });
-    }
-    grid.push(row);
-  }
+  // global
+  var grid = [];
+  utils.initColumns(grid, height, width, makeRandom);
   return grid;
 };
 
@@ -21,51 +11,5 @@ export const advanceGrid = function(grid = []) {
   let gridHeight = grid.length;
   let gridWidth = grid[0].length;
 
-  let calculateNeighbours = function(x, y) {
-    let topRow = x - 1 < 0 ? gridHeight - 1 : x - 1;
-    let bottomRow = x + 1 === gridHeight ? 0 : x + 1;
-    let leftColumn = y - 1 < 0 ? gridWidth - 1 : y - 1;
-    let rightColumn = y + 1 === gridWidth ? 0 : y + 1;
-
-    let total = 0;
-    total += grid[topRow][leftColumn].status;
-    total += grid[topRow][y].status;
-    total += grid[topRow][rightColumn].status;
-    total += grid[x][leftColumn].status;
-    total += grid[x][rightColumn].status;
-    total += grid[bottomRow][leftColumn].status;
-    total += grid[bottomRow][y].status;
-    total += grid[bottomRow][rightColumn].status;
-
-    return total;
-  };
-  let gameState = [];
-  for (let i = 0; i < gridHeight; i++) {
-    let row = [];
-    for (let j = 0; j < gridWidth; j++) {
-      let cellIsAlive = grid[i][j].status;
-      let neighbours = calculateNeighbours(i, j);
-      if (cellIsAlive) {
-        if (neighbours < 2) {
-          row.push({ status: 0 });
-        } else if (neighbours > 3) {
-          row.push({ status: 0 });
-        } else {
-          row.push({ status: 1 });
-        }
-      }
-      if (!cellIsAlive) {
-        if (neighbours === 3) {
-          row.push({
-            status: 1,
-            newBorn: true,
-          });
-        } else {
-          row.push({ status: 0 });
-        }
-      }
-    }
-    gameState.push(row);
-  }
-  return gameState;
+  return utils.advanceColumn(grid, gridHeight, gridWidth);
 };
---
+++ src/app-logic/utils.js
@@ -0,0 +1,75 @@
+// helper
+export const makeArray = (n) => {
+  return Array(n).fill(null);
+};
+
+// make grid
+export const initRows = (width, makeRandom) => {
+  return makeArray(width).map(() => {
+    let value;
+    if (makeRandom) {
+      value = Math.random() > 0.85;
+    }
+    return {
+      status: value,
+      newBorn: value,
+    };
+  });
+};
+
+export const initColumns = (grid, height, width, makeRandom) => {
+  makeArray(height).map(() => {
+    grid.push(initRows(width, makeRandom));
+  });
+};
+
+// advance grid
+export const getCellData = (isCellAlive, neighbours) => {
+  if (isCellAlive) {
+    const status = neighbours > 3 || neighbours < 2 ? 0 : 1;
+    return { status };
+  } else {
+    if (neighbours === 3) {
+      return {
+        status: 1,
+        newBorn: true,
+      };
+    } else {
+      return { status: 0 };
+    }
+  }
+};
+
+export const calculateNeighbours = (grid, x, y, gridHeight, gridWidth) => {
+  let topRow = x - 1 < 0 ? gridHeight - 1 : x - 1;
+  let bottomRow = x + 1 === gridHeight ? 0 : x + 1;
+  let leftColumn = y - 1 < 0 ? gridWidth - 1 : y - 1;
+  let rightColumn = y + 1 === gridWidth ? 0 : y + 1;
+
+  let total = 0;
+  total += grid[topRow][leftColumn].status;
+  total += grid[topRow][y].status;
+  total += grid[topRow][rightColumn].status;
+  total += grid[x][leftColumn].status;
+  total += grid[x][rightColumn].status;
+  total += grid[bottomRow][leftColumn].status;
+  total += grid[bottomRow][y].status;
+  total += grid[bottomRow][rightColumn].status;
+
+  return total;
+};
+
+const advanceRow = (grid, i, gridHeight, gridWidth) => {
+  return makeArray(gridWidth).map((_, j) => {
+    const isCellAlive = grid[i][j].status;
+    const neighbours = calculateNeighbours(grid, i, j, gridHeight, gridWidth);
+
+    return getCellData(isCellAlive, neighbours);
+  });
+};
+
+export const advanceColumn = (grid, gridHeight, gridWidth) => {
+  return makeArray(gridHeight).map((_, i) => {
+    return advanceRow(grid, i, gridHeight, gridWidth);
+  });
+};
