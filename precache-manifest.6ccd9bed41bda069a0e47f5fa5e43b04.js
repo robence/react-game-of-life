87c43418fc0ab2ab038ce3686945cdf52cfe870a
rozsabence <robence@gmail.com>
2019-02-21 15:31:49 +0100

    Updates

---
+++ precache-manifest.6ccd9bed41bda069a0e47f5fa5e43b04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "90e57bd063354564713e",
+    "url": "/react-game-of-life/static/css/main.4c888d31.chunk.css"
+  },
+  {
+    "revision": "90e57bd063354564713e",
+    "url": "/react-game-of-life/static/js/main.90e57bd0.chunk.js"
+  },
+  {
+    "revision": "4cbbc15549b1aaf479b7",
+    "url": "/react-game-of-life/static/js/runtime~main.4cbbc155.js"
+  },
+  {
+    "revision": "ebef84b1e13c15089fce",
+    "url": "/react-game-of-life/static/js/2.ebef84b1.chunk.js"
+  },
+  {
+    "revision": "7b0ab1cc80eacfc66ebb06ca5e057688",
+    "url": "/react-game-of-life/static/media/background.7b0ab1cc.jpg"
+  },
+  {
+    "revision": "82e66ccc73b41568a94f404cc77c15f6",
+    "url": "/react-game-of-life/index.html"
+  }
+];
